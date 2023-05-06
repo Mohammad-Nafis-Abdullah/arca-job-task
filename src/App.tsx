f89ff2703a929a0f73e4f5d7f731f70447908aec
Muhammad-Nafis-Abdullah <nafisabdullah2@gmail.com>
2023-05-07 03:04:37 +0600

    initially implemented by following the instruction

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import { objData } from "./utils/data";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [dataArr, setData] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(() => {
+    axios
+      .get("/api/test5m23", {
+        headers: {
+          Authorization: "Bearer 1|FWItRXH5DCAN9rjBjIhfH9KMnprvKZweoK2Jfi5T",
+        },
+      })
+      .then((res) => setData(res.data.data));
+  }, []);
+
+  const treeMaking = (d: objData) => {
+    const getChild = dataArr.filter((data: objData) => {
+      return data.parentId === d.id;
+    });
+    if (getChild.length) {
+      const childArray = getChild.map((c: objData, i) => {
+        return (
+          <li key={i}>
+            <div className="box">
+              <h3>{c.id}</h3>
+              <h5>{c.name}</h5>
+            </div>
+            <ul>{treeMaking(c)}</ul>
+          </li>
+        );
+      });
+      return childArray;
+    } else {
+      return;
+    }
+  };
+
+  const display = (arr: objData[]) => {
+    const d = arr.map((d, i) => {
+      if (d.type === "Category" && !d.parentId) {
+        return (
+          <li key={i} className="extra-gap">
+            <span className="box">
+              <h3>{d.id}</h3>
+              <h5>{d.name}</h5>
+            </span>
+            <ul>{treeMaking(d)}</ul>
+          </li>
+        );
+      } else if (d.type === "Course" && !d.parentId) {
+        const uniqueCat = arr.filter((data: objData) => {
+          return data.type === "Category" && data.category_id === d.category_id;
+        });
+        if (!uniqueCat.length) {
+          return (
+            <li key={i} className="extra-gap">
+              <span className="box">
+                <h3>{d.id}</h3>
+                <h5>{d.name}</h5>
+              </span>
+              <ul>{treeMaking(d)}</ul>
+            </li>
+          );
+        }
+      }
+    });
+    const filtered = d.filter((d) => d);
+    return filtered;
+  };
+
+  // console.log(display(dataArr));
+
+  return <ul>{display(dataArr)}</ul>;
 }
 
-export default App
+export default App;
---
+++ src/utils/data.ts
@@ -0,0 +1,12 @@
+export interface objData {
+    id:number;
+    name: string;
+    teacher: null | string;
+    category_id: number;
+    type: string;
+    description: string;
+    created_at: string;
+    updated_at: string;
+    prerequisites: string;
+    parentId: null | number;
+}
