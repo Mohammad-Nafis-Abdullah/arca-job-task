470f7e3277898f5c26c6048bfa80afd4d75c90ec
Muhammad-Nafis-Abdullah <nafisabdullah2@gmail.com>
2023-05-07 21:09:04 +0600

    hierarchy implemented according my thoughts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
-import { objData } from "./utils/data";
+import { Hierarchy } from "./components/Hierarchy";
 
 function App() {
   const [dataArr, setData] = useState([]);
@@ -16,64 +16,11 @@ function App() {
       .then((res) => setData(res.data.data));
   }, []);
 
-  const treeMaking = (d: objData) => {
-    const getChild = dataArr.filter((data: objData) => {
-      return data.parentId === d.id;
-    });
-    if (getChild.length) {
-      const childArray = getChild.map((c: objData, i) => {
-        return (
-          <li key={i}>
-            <div className="box">
-              <h3>{c.id}</h3>
-              <h5>{c.name}</h5>
-            </div>
-            <ul>{treeMaking(c)}</ul>
-          </li>
-        );
-      });
-      return childArray;
-    } else {
-      return;
-    }
-  };
-
-  const display = (arr: objData[]) => {
-    const d = arr.map((d, i) => {
-      if (d.type === "Category" && !d.parentId) {
-        return (
-          <li key={i} className="extra-gap">
-            <span className="box">
-              <h3>{d.id}</h3>
-              <h5>{d.name}</h5>
-            </span>
-            <ul>{treeMaking(d)}</ul>
-          </li>
-        );
-      } else if (d.type === "Course" && !d.parentId) {
-        const uniqueCat = arr.filter((data: objData) => {
-          return data.type === "Category" && data.category_id === d.category_id;
-        });
-        if (!uniqueCat.length) {
-          return (
-            <li key={i} className="extra-gap">
-              <span className="box">
-                <h3>{d.id}</h3>
-                <h5>{d.name}</h5>
-              </span>
-              <ul>{treeMaking(d)}</ul>
-            </li>
-          );
-        }
-      }
-    });
-    const filtered = d.filter((d) => d);
-    return filtered;
-  };
-
-  // console.log(display(dataArr));
-
-  return <ul>{display(dataArr)}</ul>;
+  return (
+    <>
+      <Hierarchy data={dataArr} />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Hierarchy.tsx
@@ -0,0 +1,99 @@
+import { objData } from "../schema/data.schema";
+
+export const Hierarchy = ({ data }: { data: objData[] }) => {
+    let totalData: objData[] = [...data];
+
+    const parent = (d: objData[]) => {
+        const parent = d.filter((p: objData) => {
+        if (!p.parentId && p.type === "Category") {
+            return true;
+        } else {
+            return false;
+        }
+        });
+
+        const parentTag = parent.map((p: objData, i) => {
+        totalData = totalData.filter((r: objData) => {
+            return r.name !== p.name;
+        });
+        return (
+            <li key={i} className="extra-gap parent">
+            <span className="box">
+                {/* <h3>{p.id}</h3> */}
+                <h5>{p.name}</h5>
+            </span>
+            <ul>{firstChild(p.id, totalData)}</ul>
+            </li>
+        );
+        });
+        return parentTag;
+    };
+
+    const firstChild = (parentId: number, d: objData[]) => {
+        const childLevel_1 = d.filter((c1: objData) => {
+        if (parentId === c1.parentId) {
+            return true;
+        } else if (
+            c1.type === "Course" &&
+            !c1.parentId &&
+            !d.find(
+            (td: objData) =>
+                td.type === "Category" && td.category_id === c1.category_id
+            )
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+        });
+
+        const childLevel_1_tag = childLevel_1.map((c1: objData, i) => {
+        totalData = totalData.filter((r: objData) => {
+            return r.name !== c1.name;
+        });
+        return (
+            <li key={i}>
+            <span className="box">
+                {/* <h3>{c1.id}</h3> */}
+                <h5>{c1.name}</h5>
+            </span>
+            <ul className="firstChild">{child(c1, totalData)}</ul>
+            </li>
+        );
+        });
+        return childLevel_1_tag;
+    };
+
+    const child = (p: objData, d: objData[]) => {
+        const getChild = d.filter((c: objData) => {
+        if (p.id === c.parentId) {
+            return true;
+        } else {
+            return false;
+        }
+        });
+
+        const child_tag = getChild.map((c: objData, i) => {
+        totalData = totalData.filter((r: objData) => {
+            return r.name !== c.name;
+        });
+        return (
+            <li key={i}>
+            <span className="box">
+                {/* <h3>{c.id}</h3> */}
+                <h5>{c.name}</h5>
+            </span>
+            <ul>{child(c, totalData)}</ul>
+            </li>
+        );
+        });
+
+        return child_tag;
+    };
+
+    return (
+        <div>
+        <ul>{parent(totalData)}</ul>
+        </div>
+    );
+};
